8547c24d29172e3c9c3dbce9a45b7a1dc79e2359
Melquisedeque Pereira <melqui1@live.com.pt>
2019-04-24 17:22:34 +0100

    Allow stylus plugins
    Allows a new parameter to specify plugin(s) to be added to stylus on package.json 
    	"scripts": {
    		"start": "react-scripts-with-stylus start src/css/index.styl nib",
    		"build": "react-scripts-with-stylus build src/css/index.styl nib",
    		"test": "react-scripts test",
    		"eject": "react-scripts eject"
    	},
    credit goes to https://github.com/syarul

--- bin/react-scripts-with-stylus.js
+++ bin/react-scripts-with-stylus.js
@@ -6,31 +6,54 @@ const spawn = require('cross-spawn');
 const script = process.argv[2];
 const input = path.join(process.cwd(), process.argv[3] || 'src');
 
+// allow use of stylus plugins
+function addStylusPlugins(args) {
+	const plugins = process.argv[4].split(',');
+	if(plugins && plugins.length){
+		plugins.map(p =>
+			args = args.concat(['--use', p])
+		);
+	}
+	return args;
+}
+
+let args;
+
 switch(script) {
-  case 'start':
-    const reactScripts = spawn('react-scripts', ['start'], {
-      stdio: 'inherit'
-    });
-
-    const stylusWatch = spawn('stylus', ['--watch', '--include-css', input], {
-      stdio: 'inherit'
-    });
-
-    break;
-
-  case 'build':
-    spawn.sync('stylus', ['--include-css', input], {
-      stdio: 'inherit'
-    });
-
-    spawn.sync('react-scripts', ['build'], {
-      stdio: 'inherit'
-    });
-
-    break;
-
-  default:
-    console.log('Unknown script: ' + script);
-    console.log('Perhaps you meant to run `react-scripts` or `react-scripts-with-stylus start`');
-    break;
+	case 'start':
+		const reactScripts = spawn('react-scripts', ['start'], {
+			stdio: 'inherit'
+		});
+		
+		args = ['--watch', '--include-css', input];
+		if(process.argv[4]){
+			args = addStylusPlugins(args);
+		}
+		
+		const stylusWatch = spawn('stylus', args, {
+			stdio: 'inherit'
+		});
+		
+		break;
+	
+	case 'build':
+		args = ['--include-css', input];
+		if(process.argv[4]){
+			args = addStylusPlugins(args);
+		}
+		
+		spawn.sync('stylus', args, {
+			stdio: 'inherit'
+		});
+		
+		spawn.sync('react-scripts', ['build'], {
+			stdio: 'inherit'
+		});
+		
+		break;
+	
+	default:
+		console.log('Unknown script: ' + script);
+		console.log('Perhaps you meant to run `react-scripts` or `react-scripts-with-stylus start`');
+		break;
 }
